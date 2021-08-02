f9a7693cd50753bd286afcdee5e38001df45ff6b
Ojan Vafai <ojan.vafai@gmail.com>
2021-08-01 17:52:12 -0700

    Add pinning and storage sync

--- page.js
+++ page.js
@@ -1,56 +1,103 @@
 const NUM_TASKS = 20;
 const TASKS_KEY = 'tasks';
+const PINNED_STATUS = 'pinned';
+const SAVE_DELAY_MS = 500;
 
-function createTasks(taskContents) {
+let tasks;
+let taskModel;
+let saveTimeout;
+
+function isPinned(task) {
+  return task.status === PINNED_STATUS;
+}
+
+function render() {
+  document.body.textContent = '';
+
+  tasks = [];
   for (let i = 0; i < NUM_TASKS; i++) {
-    const task = document.createElement('input');
-    if (i < taskContents.length) {
-      task.value = taskContents[i];
+    if (!taskModel[i]) {
+      taskModel[i] = {content: ''};
     }
-    task.addEventListener('input', handleInput);
-    document.body.append(task);
+    const task = document.createElement('input');
+    task.value = taskModel[i].content;
+    task.addEventListener('input', () => {
+      taskModel[i].content = tasks[i].value;
+      clearTimeout(saveTimeout);
+      saveTimeout = setTimeout(() => saveData(), SAVE_DELAY_MS);
+    });
+    tasks.push(task);
+
+    const pin = document.createElement('div');
+    pin.className = 'pin';
+    if (isPinned(taskModel[i])) {
+      pin.classList.add('active');
+    }
+    pin.textContent = '📌';
+    pin.addEventListener('click', () => {
+      taskModel[i].status = isPinned(taskModel[i]) ? '' : PINNED_STATUS;
+      saveData();
+    });
+
+    const row = document.createElement('div');
+    row.className = 'row';
+    row.append(pin, task);
+    document.body.append(row);
   }
 }
 
-function getTasks() {
-  return Array.from(document.querySelectorAll('input'));
+function saveData() {
+  taskModel.sort((a, b) => {
+    const aPinned = isPinned(a)
+    const bPinned = isPinned(b);
+    return aPinned && !bPinned ? -1 : !aPinned && bPinned ? 1 : 0;
+  });
+  chrome.storage.sync.set({[TASKS_KEY]: taskModel});
+  render();
 }
 
-function handleInput() {
-  const data = getTasks().map(x => x.value).filter(x => x !== '');
-  chrome.storage.sync.set({[TASKS_KEY]: data});
-}
-
-function move(moveDown) {
+function move(amount) {
   const focused = document.activeElement;
   let toFocus;
   if (!focused || focused.tagName !== 'INPUT') {
     toFocus = document.querySelector('input');
   } else {
-    toFocus = moveDown ? focused.nextSibling : focused.previousSibling;
+    toFocus = tasks[tasks.findIndex(x => x === focused) + amount];
     if (!toFocus) {
       return;
     }
   }
-  toFocus.focus();
+  // Not really sure why this setTimeout is needed, but otherwise the setSelectionRange call doens't work.
+  setTimeout(() => {
+    toFocus.setSelectionRange(0, 0);
+    toFocus.focus();
+  }, 0);
 }
 
-function refreshTasks() {
-  chrome.storage.sync.get([TASKS_KEY], (data) => {
-    document.body.textContent = '';
-    createTasks(data[TASKS_KEY] || []);
+function refreshTasksFromStorage() {
+  chrome.storage.sync.get([TASKS_KEY], (newData) => {
+    const newTasks = newData && newData[TASKS_KEY] || [];
+    if (JSON.stringify(taskModel) !== JSON.stringify(newTasks)) {
+      taskModel = newTasks;
+      render();
+    }
   });
 }
 
 window.onload = () => {
-  refreshTasks();
-  document.addEventListener("visibilitychange", e => document.visibilityState === 'hidden' && refreshTasks());
-
+  refreshTasksFromStorage();
+  document.addEventListener("visibilitychange", e => {
+    if (document.visibilityState !== 'hidden') {
+      taskModel = taskModel.filter(x => x.content !== '')
+      saveData();
+    }
+  });
+  chrome.storage.onChanged.addListener(() => refreshTasksFromStorage());
   document.body.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowUp') {
-      move(false);
-    } else if (e.key === 'ArrowDown') {
-      move(true);
+    switch (e.key) {
+      case 'ArrowUp':
+      case 'ArrowDown':
+        move(e.key === 'ArrowUp' ? -1 : 1);
     }
   });
 }
